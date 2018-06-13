bb76e486f727067e804010f2c19dc23c8525cf8f
AIE\s162866 <contact@trentwilliamswanson.com>
2018-06-13 15:57:08 +1000

    added grid generation

---
+++ Any Means Necessary/Assets/GenerateGrid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GenerateGrid : MonoBehaviour {
+
+    public GameObject tilePrefab;
+
+    public Vector2 gridSize;
+
+    [Range(0,1)]
+    public float tilePadding;
+
+    [ContextMenu("GenerateGrid")]
+
+    private void Start() {
+        GameManager.grid = new GameObject[(int)gridSize.x, (int)gridSize.y];
+    }
+
+    public void Generate() {
+
+        string holderName = "Generate Grid";
+        if (transform.Find(holderName)) {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform gridHolder = new GameObject(holderName).transform;
+        gridHolder.parent = transform;
+
+        for (int x = 0; x < gridSize.x; x++) {
+            for (int y = 0; y < gridSize.y; y++) {
+                Vector3 tilePosition = new Vector3(-gridSize.x / 2 + 0.5f + x, 0.01f, -gridSize.y / 2 + 0.5f + y);
+                GameObject newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right*90));
+                newTile.transform.localScale = Vector3.one*(1 - tilePadding);
+                newTile.transform.SetParent(gridHolder);
+                GameManager.grid[x, y] = newTile;
+            }
+        }
+    }
+}
---
+++ Any Means Necessary/Assets/GridBuilderEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GenerateGrid))]
+public class GridBuilderEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        GenerateGrid map = target as GenerateGrid;
+        map.Generate();
+    }
+}
